7efbaf6327635d08f0705b7d4fa94cafeab67562
Dat Florentin <floren_d@epita.fr>
2016-03-29 11:55:38 +0200

    fact files

---
+++ src/client/exercise-02/ng/components/fact.js
@@ -0,0 +1,10 @@
+window._app.global_ng_module
+.component('fact', {
+  templateUrl: 'client/exercise-02/ng/components/fact.html',
+  bindings: {
+    fact: '='
+  },
+  controller: ['$scope', function($scope) {
+
+  }]
+});
