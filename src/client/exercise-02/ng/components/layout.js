451f0ca9b2c2a58bed99b6d3120230572cfe4811
Dat Florentin <floren_d@epita.fr>
2016-03-29 11:55:03 +0200

    exercise 2

--- src/client/exercise-02/ng/components/layout.js
+++ src/client/exercise-02/ng/components/layout.js
@@ -1,11 +1,14 @@
 import 'client/exercise-02/ng/services/chuck-norris';
+import 'client/exercise-02/ng/components/fact';
 
 window._app.global_ng_module
 .component('layout', {
   templateUrl: 'client/exercise-02/ng/components/layout.html',
-  controller: ['chuckNorris', function(chuckNorris) {
+  controller: ['chuckNorris','$scope', function(chuckNorris, $scope) {
+
     chuckNorris.fetch3Random().then((res) => {
       console.log(res.data.value);
+      $scope.facts = res.data.value;
     })
   }]
 });
