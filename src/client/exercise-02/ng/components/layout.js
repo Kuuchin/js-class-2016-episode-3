d4ac2e5792cf620a7735c464822a0828f51f1a68
Dat Florentin <floren_d@epita.fr>
2016-03-29 12:54:10 +0200

    removed scope

--- src/client/exercise-02/ng/components/layout.js
+++ src/client/exercise-02/ng/components/layout.js
@@ -4,11 +4,11 @@ import 'client/exercise-02/ng/components/fact';
 window._app.global_ng_module
 .component('layout', {
   templateUrl: 'client/exercise-02/ng/components/layout.html',
-  controller: ['chuckNorris','$scope', function(chuckNorris, $scope) {
+  controller: ['chuckNorris', function(chuckNorris) {
 
     chuckNorris.fetch3Random().then((res) => {
       console.log(res.data.value);
-      $scope.facts = res.data.value;
+      this.facts = res.data.value;
     })
   }]
 });
